6b8782ec5ca3aa4ed3442f07ef0cbc255d165b49
Francis Rohner <Francis Rohner>
2017-01-16 18:50:50 -0500

    Added ability to jump

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -14,8 +14,14 @@ public class Player : MonoBehaviour {
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
+        
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 200, 0);
+        }
+
 	}
 }
